3c8eaac363da85ac88499574f9deffd3f92453b4
spoonLead <xuxlaevoleg@yandex.ru>
2020-12-02 17:45:06 +0300

    Feat: Added arrays and arrays filling code

---
+++ BliniGamesTestTask/BliniGamesTestTask/GameObject.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BliniGamesTestTask
+{
+    class GameObject
+    {
+    }
+}
---
+++ BliniGamesTestTask/BliniGamesTestTask/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BliniGamesTestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameObject[] firstArray = new GameObject[5];
+            firstArray[0] = new GameObject();
+            firstArray[1] = null;
+            firstArray[2] = new GameObject();
+            firstArray[3] = null;
+            firstArray[4] = null;
+
+            GameObject[] secondArray = new GameObject[3];
+            secondArray[0] = new GameObject();
+            secondArray[1] = new GameObject();
+            secondArray[2] = new GameObject();
+
+
+
+        }
+    }
+}
